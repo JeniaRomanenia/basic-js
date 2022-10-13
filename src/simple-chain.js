cc19eac907b2481fddcbdd1b9c5e5954ffa07512
Jenia Romanenia <71782573+JeniaRomanenia@users.noreply.github.com>
2022-10-13 17:23:27 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+      this.chain.push(`(${value})`);
+      return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+      if (!Number.isInteger(position)) {
+          this.chain = [];
+          return new Error('Error! Not an integer!');
+      }
+      if (position < 1 || position > this.chain.length){
+          this.chain = [];
+          return new Error('Error! Enter another number!');
+      }
+          this.chain.splice(position - 1, 1);
+      return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      let endChain = this.chain.join(' ~~ ');
+      this.chain = [];
+      return endChain;
   }
 };
 
